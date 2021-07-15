cbc4db3d344e5c7050730e0158c09634639440da
rockshang <s2265681@163.com>
2021-07-15 22:12:46 +0800

    feat:配置

--- src/pages/KonvaBoard/index.tsx
+++ src/pages/KonvaBoard/index.tsx
@@ -1,78 +1,94 @@
-import { Button } from 'antd';
-import React, { FC, useEffect,useRef } from 'react';
-import { router } from "dva";
-import Konva from 'konva';
-// import Konva from 'konva/lib/Core';
-// import { Rect } from 'konva/lib/shapes/Rect';
-const { Link } = router;
+import { Button } from "antd";
+import React, { FC, useEffect, useRef } from "react";
+import { Stage, Layer, Star, Text, Rect, Circle, Line } from "react-konva";
+
+function generateShapes() {
+  return [...Array(10)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
 
 const KonvaBoard: FC = () => {
-
-  const canvasEl = useRef(null)
-  useEffect(()=>{
-    // var rect1 = new Rect();
-    // var shape = new Konva.Rect();
-    // canvasEl.current
-    // const canvas = new Konva
-     initCanvas()
-  },[])
-
-
-  function initCanvas(){
-    let stage = new Konva.Stage({
-      container: 'konva-canvas',
-      width: window.innerWidth,
-      height: window.innerHeight,
-    })
-  
-     // add canvas element
-     let layer = new Konva.Layer();
-   
-     // create shape
-     var box = new Konva.Rect({
-       x: 50,
-       y: 50,
-       width: 100,
-       height: 50,
-       fill: '#00D2FF',
-       stroke: 'black',
-       strokeWidth: 4,
-       draggable: true,
-     });
-
-     layer.add(box);
-
-     stage.add(layer);
-
-     console.log(stage,'stage')
-    
-  // add cursor styling
-  // box.on('mouseover', function () {
-  //   document.body.style.cursor = 'pointer';
-  // });
-  // box.on('mouseout', function () {
-  //   document.body.style.cursor = 'default';
-  // });
-    }
-
-    
+  const [stars, setStars] = React.useState(INITIAL_STATE);
+  const handleDragStart = (e: any) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e: any) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
 
   return (
-    <div>
-       <Button type="primary">
-          <Link to="/overview/all">返回</Link>
-      </Button>
-       KonvaBoard
-       <br/>
-       <br/>
-       <canvas
-        style={{ border: "1px solid #f00"}}
-        ref={canvasEl}
-        id="konva-canvas"
-        width="1000"
-        height="1000"
-      />
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
+
+        <Rect
+          x={20}
+          y={50}
+          width={100}
+          height={100}
+          fill="red"
+          shadowBlur={10}
+        />
+        <Circle x={200} y={100} radius={50} fill="green" />
+        <Line
+          x={20}
+          y={200}
+          points={[0, 0, 100, 0, 100, 100]}
+          tension={0.5}
+          closed
+          stroke="black"
+          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+          fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
+        />
+      </Layer>
+    </Stage>
   );
 };
 
